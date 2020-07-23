e97be1f8b4c492f42246cfe2df1f9162c3804e6d
Preston Patterson <prestonjpatterson@gmail.com>
2020-07-23 00:46:30 -0600

    added some of the funchanity

---
+++ 02-Homework/Develop/app.js
@@ -0,0 +1,104 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+function Emmploy(name, role, email, special) {
+    this.name
+    this.role
+    this.email
+    this.special
+
+    this.makeCard = () => {
+
+    }
+}
+
+
+const ackQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "What role do you need?",
+            choices: ["Manager", "Engineer", "Intern", "I don't need more."]
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What is their name?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email?"
+        },
+
+    ])
+        .then((answers) => {
+            switch (answers.role) {
+                case "Manager":
+                    return inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "officeNumber",
+                            message: "What is their office number?"
+                        },
+                    ])
+                    break;
+                case "Engineer":
+                    return inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "github",
+                            message: "What is their github username?"
+                        },
+                    ])
+                    break;
+                case "Intern":
+                    return inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "school",
+                            message: "Where is their school?"
+                        },
+                    ])
+                    break;
+                default:
+                    break;
+            }
+        })
+}
+
+
+
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
